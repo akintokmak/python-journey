672e84ff655a80408465a81b7344760ea4e32b50
AKIN TOKMAK <akinntok.45@gmail.com>
2026-03-04 13:40:13 +0300

    feat: restore project files and finalize structure

---
+++ Day8_Practise/planet_and_cargo_list.py
@@ -0,0 +1,8 @@
+planet_list = ["Mars",
+               "Jupiter",
+               "Moon"]
+cargo_list = ["Furniture",
+              "Trash"]
+
+dangerous_cargo_list = ["Nuclear",
+                        "Explosive"]
